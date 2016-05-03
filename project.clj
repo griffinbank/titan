678ea0b03e7ec37b103dd0cd600fb102a1ceee15
Ursa Americanus Kermodei <venantius@gmail.com>
2016-05-02 21:06:31 -0700

    [titan] Drop Clojure version to 1.7.0
    This is temporary, but until more libraries are explicitly able to
    support Clojure 1.8.0 it's better to support 1.7.0

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.logging "0.3.1"]
                  [org.clojure/tools.nrepl "0.2.12"]
                  [org.slf4j/slf4j-log4j12 "1.7.21"]
