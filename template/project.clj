a5559bc273a3754445c671e51b6326aa4bc92f86
David Jarvis <venantius@gmail.com>
2017-12-08 19:43:37 +0000

    [titan] Adding description and url to template

--- template/project.clj
+++ template/project.clj
@@ -1,5 +1,6 @@
 (defproject titan-app/lein-template "0.0.1alpha4"
-  :url "http://example.com/FIXME"
+  :description "A Leiningen template for a Titan web application."
+  :url "http://github.com/venantius/titan"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
